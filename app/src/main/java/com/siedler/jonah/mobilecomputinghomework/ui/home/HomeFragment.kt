1dc4d69708a9e760644215f6efdb95ed8c56d390
Jonah Siedler <jonah.siedler@gmail.com>
2022-01-31 18:45:52 +0200

    add a spacer as last element of the list to prevent the action button to cover it

--- app/src/main/java/com/siedler/jonah/mobilecomputinghomework/ui/home/HomeFragment.kt
+++ app/src/main/java/com/siedler/jonah/mobilecomputinghomework/ui/home/HomeFragment.kt
@@ -15,9 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
@@ -51,11 +48,15 @@ class HomeFragment : Fragment() {
 
     @Composable
     fun MessageList(messages: List<Reminder>) {
-        LazyColumn {
+        LazyColumn(
+        ) {
             items(1) {
                 messages.forEach { reminder ->
                     MessageRow(reminder)
                 }
+                Spacer(
+                    modifier = Modifier.height(100.dp)
+                )
             }
         }
     }
